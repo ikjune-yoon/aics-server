c2eb396a52b4282807aa2451049f59b8ba6832bb
Minjoon Park <alswns11346@gmail.com>
2024-10-28 23:00:44 +0900

    feature/#46 게시글 수정 api 구현 (#48)
    * feat: 게시글 수정 요청 DTO 생성
    
    * feat: 게시글 ID를 통해 데이터 조회 기능 구현
    
    * feat: 게시글 수정 권한 및 데이터 검증을 위한 커스텀 예외 구현
    
    * feat: 게시글 수정 서비스 로직 구현
    
    * feat: 게시글 수정 요청을 처리하는 API 컨트롤러 구현
    
    * refactor: 게시글 수정 API 리턴 타입 변경
    
    PostPersistResponse -> Void
    
    * refactor: 게시글 요청 관련 DTO 메세지 삭제
    
    * refactor: 게시글 조회 커스텀 에러 수정
    
    * refactor: 작성자 검증 로직 제거
    
    * refactor: 게시글 작성, 수정 DTO 통합 및 @Parameter 예시 추가

--- aics-api/src/main/java/kgu/developers/api/post/application/PostService.java
+++ aics-api/src/main/java/kgu/developers/api/post/application/PostService.java
@@ -4,10 +4,11 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
-import kgu.developers.api.post.presentation.request.PostCreateRequest;
-import kgu.developers.api.user.application.UserService;
+import kgu.developers.api.post.presentation.exception.PostNotFoundException;
+import kgu.developers.api.post.presentation.request.PostRequest;
 import kgu.developers.api.post.presentation.response.PostPersistResponse;
 import kgu.developers.api.post.presentation.response.PostSummaryPageResponse;
+import kgu.developers.api.user.application.UserService;
 import kgu.developers.common.response.PaginatedListResponse;
 import kgu.developers.domain.post.domain.Post;
 import kgu.developers.domain.post.domain.PostRepository;
@@ -21,7 +22,7 @@ public class PostService {
 	private final UserService userService;
 
 	@Transactional
-	public PostPersistResponse createPost(PostCreateRequest request) {
+	public PostPersistResponse createPost(PostRequest request) {
 		User author = userService.me();
 		Post createPost = Post.create(request.title(), request.content(), author);
 		postRepository.save(createPost);
@@ -34,10 +35,18 @@ public class PostService {
 			PaginatedListResponse<Post> paginatedListResponse = postRepository.findAllByTitleContainingOrderByCreatedAtDesc(
 				keyword, request);
 			return PostSummaryPageResponse.of(paginatedListResponse.contents(), paginatedListResponse.pageable());
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
+	}
+
+	@Transactional
+	public void updatePost(Long postId, PostRequest request) {
+		Post updatePost = postRepository.findById(postId)
+			.orElseThrow(PostNotFoundException::new);
+
+		updatePost.updateTitle(request.title());
+		updatePost.updateContent(request.content());
 	}
 }
--- aics-api/src/main/java/kgu/developers/api/post/presentation/request/PostRequest.java
+++ aics-api/src/main/java/kgu/developers/api/post/presentation/request/PostRequest.java
@@ -6,14 +6,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record PostCreateRequest(
+public record PostRequest(
 	@Schema(description = "게시물 제목", example = "박민준은 살아있다", requiredMode = REQUIRED)
-	@NotBlank(message = "제목은 필수입니다.")
+	@NotBlank
 	@Size(max = 100, message = "제목은 100자 이내여야 합니다.")
 	String title,
 
 	@Schema(description = "게시물 내용", example = "살아 숨셔", requiredMode = REQUIRED)
-	@NotBlank(message = "내용은 필수입니다.")
+	@NotBlank
 	String content
 ) {
 }
